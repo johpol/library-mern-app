7f6d4acba8fa8ad68bbc31244c75b5bd9de6418c
johpol <johpol@users.noreply.github.com>
2018-05-01 20:26:20 -0400

    Initial Commit

---
+++ README.md
@@ -0,0 +1,27 @@
+# Introduction
+This is a library application that allows the end user to keep track of their own books. The application accepts a ten digit ISBN as input, fetches the information using a web service and stores that information in persistent storage. The list is then displayed to the user with the option to remove the book from their collection. This is implemented as a single page application providing the user with a seamless and snappy experience. 
+
+# Goal
+The goal is to develop a new front-end using the React framework in order to compare and contrast other JavaScripot frameworks.
+
+# Technologies Used
+- MongoDB
+- React.JS
+- Express.JS
+- Node.JS
+- JavaScript
+- Bootstrap
+- Node Package Manager
+- Bower
+
+# Installation
+## Node.JS Application
+### In the root directory:
+```
+> bower install
+> npm install
+> npm start
+```
+Navigate to localhost:3000
+
+_Note: It is assumed that there is a MongoDB instance running locally._
---
+++ routes/index.js
@@ -0,0 +1,100 @@
+var express = require('express');
+var router = express.Router();
+var mongoose = require('mongoose');
+var nodeIsbn = require('node-isbn');
+var path = require('path');
+
+var options = {useMongoClient: true};
+mongoose.connect('mongodb://localhost/LibraryApp', options); 
+
+var bk = mongoose.model('Book',
+    new mongoose.Schema({
+        type: String,
+        attributes: {
+            isbn: String,
+            title: String,
+            author: String,
+            publisher: String,
+            subject: String
+        }
+    }),
+    'books_with_isbn_jsonapi'
+);
+
+/* GET home page. */
+router.get('/', function (req, res) {
+    res.sendFile(path.resolve('./LibraryEmberApp/dist/index.html'))
+});
+
+router.get('/books', function(req, res) {
+    bk.find(function(err, book) {
+        if(err) {
+            res.send(err)
+        }
+
+        res.json({'data':book});
+    });
+});
+
+router.delete('/books/:book_id', function (req, res) {
+    console.log(req.params);
+    bk.remove({
+        _id: req.params.book_id
+    }, function (err, book) {
+        if (err) {
+            res.send(err);
+        }
+    })
+    
+    res.status(204).send();
+});
+
+router.post('/books', function (req, res) {
+    console.log(req.body);
+    var attributes = req.body.data.attributes;
+
+    nodeIsbn.resolve(attributes.isbn, function (err, isbnBook) {
+        if (err) {
+            var error = {
+                "errors": [
+                    {
+                        "status": "422",
+                        "code": "Invalid ISBN",
+                        "title": "Invalid ISBN",
+                        "detail": err.toString(),
+                        "source": {
+                            "pointer": "/data/book/isbn/"
+                        }
+                    }
+                ]
+            };
+            console.log('Book not found', err);
+            res.status(422).send(error);
+        } else {
+            console.log('Book found %j', isbnBook);
+            bk.create({
+                type: 'books',
+                attributes: {
+                    title: isbnBook.title,
+                    author: isbnBook.authors,
+                    publisher: isbnBook.publisher,
+                    subject: isbnBook.categories,
+                    isbn: isbnBook.industryIdentifiers[0].identifier
+                }
+            }, function (err, newBook) {
+                if (err) {
+                    res.send(err);
+                }
+
+                bk.find({ _id: newBook._id }, function (err, book) {
+                    if (err) {
+                        res.send(err);
+                    }
+                    res.json({ data: book[0] });
+                });
+            });
+        }
+    });
+});
+
+module.exports = router;
