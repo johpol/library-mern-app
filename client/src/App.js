f10d650ddb68b9e629e17a94f0b4a249e075de28
johpol <johpol@users.noreply.github.com>
2018-06-19 20:54:24 -0400

    input group

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,20 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { Button, Alert, Container, InputGroup, InputGroupAddon, InputGroupButton, Input } from 'reactstrap';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <Container>
+          <h1>Personal Library</h1>
+          <InputGroup>
+            <Input placeholder="ISBN-10" />
+            <InputGroupAddon addonType="prepend">
+            <InputGroupButton>Fetch My Book!</InputGroupButton></InputGroupAddon>
+          </InputGroup>
+        </Container>
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
