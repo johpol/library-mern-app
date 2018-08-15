08c0aabbd74fca964fe84ead0c006d171da3e899
johpol <johpol@users.noreply.github.com>
2018-08-14 20:49:18 -0400

    dummy book data and rendering in table

--- client/src/App.js
+++ client/src/App.js
@@ -3,14 +3,57 @@ import './App.css';
 import { Button, Container, InputGroup, InputGroupAddon, Input, Table } from 'reactstrap';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { books: [] };
+  }
+
+  componentWillMount() {
+    this.setState({
+      books: [
+        {
+          title: 'title1',
+          author: 'author1',
+          publisher: 'pub1',
+          subject: 'sub1',
+          iSBN: 'isbn1'
+        },
+        {
+          title: 'title2',
+          author: 'author2',
+          publisher: 'pub2',
+          subject: 'sub2',
+          iSBN: 'isbn2'
+        }
+      ]
+    });
+  }
+
   render() {
+    const books = this.state.books.map((book) => {
+      return (
+        <tr>
+          <td>{book.title}</td>
+          <td>{book.author}</td>
+          <td>{book.publisher}</td>
+          <td>{book.subject}</td>
+          <td>{book.iSBN}</td>
+          <td>
+          <Button color='danger'>Remove</Button>
+            </td>
+        </tr>
+      )
+    });
+
     return (
       <div className="App">
         <Container>
           <h1>Personal Library</h1>
           <InputGroup>
             <Input placeholder="ISBN-10" />
-            <InputGroupAddon addonType="append"><Button color="primary">Fetch My Book!</Button></InputGroupAddon>
+            <InputGroupAddon addonType="append">
+              <Button color="primary">Fetch My Book!</Button>
+            </InputGroupAddon>
           </InputGroup>
           <Table striped>
             <thead>
@@ -24,6 +67,7 @@ class App extends Component {
               </tr>
             </thead>
             <tbody>
+              {books}
             </tbody>
           </Table>
         </Container>
